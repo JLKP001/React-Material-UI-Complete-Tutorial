89aeb0d4afda790eb388682c0381bf6ee43ed3fd
Jonathan Lim <jl14274@my.bristol.ac.uk>
2020-12-29 10:43:28 +0000

    Added custom theme to App

--- material-ui-tutorial/src/App.js
+++ material-ui-tutorial/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
 import { SideMenu } from "./components/SideMenu";
 import { Header } from "./components/Header";
-import { CssBaseline, makeStyles } from "@material-ui/core";
+import {
+  createMuiTheme,
+  CssBaseline,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: "#333996", light: "#3c44b126" },
+    secondary: { main: "#f83245", light: "#f8324526" },
+    background: { default: "#f4f5fd" },
+  },
+});
 
 const useStyles = makeStyles({
   appMain: {
@@ -13,13 +26,13 @@ const useStyles = makeStyles({
 const App = () => {
   const classes = useStyles();
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <SideMenu />
       <div className={classes.appMain}>
         <Header />
       </div>
       <CssBaseline />
-    </>
+    </ThemeProvider>
   );
 };
 
--- material-ui-tutorial/src/components/Header.js
+++ material-ui-tutorial/src/components/Header.js
@@ -13,18 +13,18 @@ import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
 import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew";
 import SearchIcon from "@material-ui/icons/Search";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: "#fff",
   },
   searchInput: {
     opacity: "0.6",
-    padding: "0px 8px",
+    padding: `0px ${theme.spacing(1)}px`,
     fontSize: "0.8rem",
     "&:hover": { backgroundColor: "#f2f2f2" },
-    "& .MuiSvgIcon-root": { marginRight: "8px" },
+    "& .MuiSvgIcon-root": { marginRight: theme.spacing(1) },
   },
-});
+}));
 
 export const Header = () => {
   const classes = useStyles();
