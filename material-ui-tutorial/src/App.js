4ed86ede60ca09a368dc7610100d2c36f9e0d858
Jonathan Lim <jl14274@my.bristol.ac.uk>
2020-12-29 10:59:17 +0000

    Added a pageHeader component

--- material-ui-tutorial/src/App.js
+++ material-ui-tutorial/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { SideMenu } from "./components/SideMenu";
 import { Header } from "./components/Header";
+import { PageHeader } from "./components/PageHeader";
 import {
   createMuiTheme,
   CssBaseline,
   makeStyles,
   ThemeProvider,
 } from "@material-ui/core";
+import PeopleOutlineTwoToneIcon from "@material-ui/icons/PeopleOutlineTwoTone";
 
 const theme = createMuiTheme({
   palette: {
@@ -30,6 +32,11 @@ const App = () => {
       <SideMenu />
       <div className={classes.appMain}>
         <Header />
+        <PageHeader
+          title="Page Header"
+          subtitle="Page Description"
+          icon={<PeopleOutlineTwoToneIcon />}
+        />
       </div>
       <CssBaseline />
     </ThemeProvider>
